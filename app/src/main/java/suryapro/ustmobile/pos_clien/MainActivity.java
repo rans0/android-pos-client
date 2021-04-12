9c4136abe780fce2d50519f2cf8785688f0ae9d6
dekran <41975320+rans0@users.noreply.github.com>
2021-04-12 21:56:31 +0700

    QR Scanner done
    Co-authored-by: zaldi-99 <rizkyzaldi@gmail.com>

--- app/src/main/java/suryapro/ustmobile/pos_clien/MainActivity.java
+++ app/src/main/java/suryapro/ustmobile/pos_clien/MainActivity.java
@@ -2,13 +2,43 @@ package suryapro.ustmobile.pos_clien;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnLihatMenu, btnCheckout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLihatMenu = findViewById(R.id.btnLihatMenu);
+        btnCheckout = findViewById(R.id.btnCheckout);
+
+        btnLihatMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMenu();
+            }
+        });
+
+        btnCheckout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openCheckout();
+            }
+        });
+    }
+    private void openMenu() {
+        Intent intent = new Intent(this, MenuActivity.class);
+        startActivity(intent);
+    }
+
+    private void openCheckout() {
+        Intent intent = new Intent(this, CheckoutActivity.class);
+        startActivity(intent);
     }
 }
